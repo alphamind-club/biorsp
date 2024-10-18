f8b50c6cb6abdfbf074d4c22cd31ffdd2945e47c
Zeyu Yao <novodoodle@gmail.com>
2024-10-19 01:48:28 +0800

    refactor: add bioRSP package initialization file and future module ideas

---
+++ src/biorsp/__init__.py
@@ -0,0 +1,13 @@
+from .preprocessing import *
+from .analysis import *
+from .visualization import *
+
+# TODO: FUTURE MODULE IDEAS FOR PACKAGE
+# from .data import *
+# from .model import *
+# from .pipeline import *
+# from .simulation import *
+# from .utils import *
+# from .plot import *
+# from .metrics import *
+# from .evaluation import *
--- src/biorsp/preprocessing/dimensionality_reduction.py
+++ src/biorsp/preprocessing/dimensionality_reduction.py
@@ -3,19 +3,22 @@ from sklearn.manifold import TSNE
 from umap import UMAP
 
 
-def run_tsne(dge_matrix_filtered, random_state=42, save_path=None):
+def run_tsne(dge_matrix_filtered, random_state=42, perplexity=30, save_path=None):
     """
     Run t-SNE on the filtered data.
 
     Parameters:
     - dge_matrix_filtered: A dataframe containing the filtered data.
     - random_state: The random state for reproducibility.
+    - perplexity: The perplexity parameter for t-SNE.
     - save_path: Path to save the t-SNE results.
 
     Returns:
     - tsne_results: A 2D numpy array with the t-SNE (or UMAP) coordinates for each cell.
     """
-    tsne = TSNE(n_components=2, perplexity=30, random_state=random_state, max_iter=1000)
+    tsne = TSNE(
+        n_components=2, perplexity=perplexity, random_state=random_state, max_iter=1000
+    )
     tsne_results = tsne.fit_transform(dge_matrix_filtered.T)
 
     if save_path:
@@ -25,19 +28,25 @@ def run_tsne(dge_matrix_filtered, random_state=42, save_path=None):
     return tsne_results
 
 
-def run_umap(dge_matrix_filtered, random_state=42, save_path=None):
+def run_umap(
+    dge_matrix_filtered, random_state=42, n_neighbors=15, min_dist=0.1, save_path=None
+):
     """
     Run UMAP on the filtered data.
 
     Parameters:
     - dge_matrix_filtered: A dataframe containing the filtered data.
     - random_state: The random state for reproducibility.
+    - n_neighbors: The number of neighbors for UMAP.
+    - min_dist: The minimum distance for UMAP.
     - save_path: Path to save the UMAP results.
 
     Returns:
     - umap_results: A 2D numpy array with the UMAP coordinates for each cell.
     """
-    umap_reducer = UMAP(n_neighbors=15, min_dist=0.1, random_state=random_state)
+    umap_reducer = UMAP(
+        n_neighbors=n_neighbors, min_dist=min_dist, random_state=random_state
+    )
     umap_results = umap_reducer.fit_transform(dge_matrix_filtered.T)
 
     if save_path:
